38f53c5e525b5c3043ac04958bd56c27e3ea33a0
ahmed <ahmedhesham2020>
2021-04-27 20:45:17 +0200

    after study sec 1

---
+++ section-1.py
@@ -0,0 +1,18 @@
+"""
+Here we define multiple
+lines comment which like the next section
+"""
+# Here are multiple
+# lines comments too
+
+fruits = ["Apple" ,"Orange" ,"Banana" ,"Watermelon"]
+# unpack list to variables
+apple ,orange ,banana ,watermelon = fruits
+# multiple initialization
+pinapple ,strawberry = "Pinapple" ,"Strawberry"
+
+print(apple)
+print(orange)
+print(pinapple)
+print(watermelon)
+print(strawberry)
