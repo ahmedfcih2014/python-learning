07195f3f2b6bef65982a8850b7bb403e527840d4
ahmed <ahmedhesham2020>
2021-05-13 17:48:46 +0200

    study chapter 7

---
+++ lesson-7.py
@@ -0,0 +1,13 @@
+# iterator : An iterator is an object that contains a countable number of values.
+# check this for more information : https://www.w3schools.com/python/python_iterators.asp
+
+print("Modules")
+
+# importing from dir dir-name.file-name
+import modules.module1 as m1
+
+m1.greet("Ahmed Hesham")
+print(m1.ali)
+
+# for dealing with dates : https://www.w3schools.com/python/python_datetime.asp
+# for dealing with math : https://www.w3schools.com/python/python_math.asp
---
+++ modules/module1.py
@@ -0,0 +1,8 @@
+def greet(name):
+    print('Hello ' + str(name))
+
+ali = {
+    'name': 'Ali Hesham',
+    'age': 14,
+    'gender': 'Male'
+}
