0e071e6053b7a0e6d40046be696517bd06106cf0
ahmed <ahmedhesham2020>
2021-05-15 18:19:00 +0200

    read about file handling

---
+++ lesson-9.py
@@ -0,0 +1,6 @@
+f = open("files.txt", "r")
+[print(l) for l in f]
+f.close()
+
+# python have a lot of file handling like modes ,read ,write create or even delete
+# visit this link for more help : https://www.w3schools.com/python/python_file_handling.asp
